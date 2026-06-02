d989f91e38288ce9579010dbc4c77a88bd2a14c2
RazzaTitian <zeusprototype01@gmail.com>
2026-06-02 14:13:07 +0700

    feat: implement CSV persistence for books

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,18 @@
+#include "models/Book.hpp"
 #include "repositories/CsvBookRepository.hpp"
 
 #include <iostream>
 
 int main() {
-    CsvBookRepository repository("data/books.csv");
+    CsvBookRepository bookRepository("data/books.csv");
 
-    repository.save(
-        Book(
-            1,
-            "Clean Code",
-            "Robert C. Martin",
-            true
-        )
-    );
-
-    repository.save(
-        Book(
-            2,
-            "Effective Modern C++",
-            "Scott Meyers",
-            true
-        )
-    );
+    bookRepository.save(Book(1, "Clean Code", "Robert C. Martin", true));
+    bookRepository.save(Book(2, "Effective Modern C++", "Scott Meyers", true));
+    bookRepository.save(Book(3, "Design Patterns", "Gamma, Helm, Johnson, Vlissides", true));
 
     std::cout << "Books currently stored:\n\n";
 
-    for (const Book& book : repository.listAll()) {
+    for (const Book& book : bookRepository.listAll()) {
         std::cout << book << '\n';
     }
 

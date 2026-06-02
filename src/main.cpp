2501ecf92a9d8a387e26b12e610aca13ec0fa106
RazzaTitian <zeusprototype01@gmail.com>
2026-06-02 14:16:16 +0700

    feat: add book persistence demo

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ int main() {
 
     bookRepository.save(Book(1, "Clean Code", "Robert C. Martin", true));
     bookRepository.save(Book(2, "Effective Modern C++", "Scott Meyers", true));
-    bookRepository.save(Book(3, "Design Patterns", "Gamma, Helm, Johnson, Vlissides", true));
+    bookRepository.save(Book(3, "Design Patterns", "Gamma Helm Johnson Vlissides", true));
 
     std::cout << "Books currently stored:\n\n";
 
